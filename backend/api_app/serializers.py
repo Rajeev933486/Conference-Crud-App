22573193f1093297e09a65abd978247bdff6825a
Rajeev933486 <95037163+Rajeev933486@users.noreply.github.com>
2025-02-27 11:39:02 +0530

    Delete backend directory

--- backend/api_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from api_app.models import Book
- 
-class BookSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Book
-        fields = ['book_id','book_title','book_author','published_date']
